5ae60253382f81ffe4a7244aa1c8d3de3e2e2d17
nbdy <me@nbdy.io>
2022-04-16 04:20:26 +0200

    added web ui changed database

--- go.mod
+++ go.mod
@@ -3,5 +3,9 @@ module dhtgo
 go 1.16
 
 require github.com/boramalper/magnetico v0.12.0
-require go.mongodb.org/mongo-driver v1.8.0
 
+require (
+	github.com/labstack/echo/v4 v4.7.2
+	github.com/noirbizarre/gonja v0.0.0-20200629003239-4d051fd0be61 // indirect
+	github.com/ostafen/clover v1.0.1
+)
--- main.go
+++ main.go
@@ -1,40 +1,80 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/boramalper/magnetico/cmd/magneticod/bittorrent/metadata"
 	"github.com/boramalper/magnetico/cmd/magneticod/dht"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"github.com/labstack/echo/v4"
+	"github.com/noirbizarre/gonja"
+	"github.com/ostafen/clover"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
-func hasInfoHash(collection *mongo.Collection, InfoHash [20]byte) bool {
-	c, _ := collection.CountDocuments(context.TODO(), bson.D{{"InfoHash", InfoHash}})
-	return c != 0
-}
+var db, _ = clover.Open("dhtdb")
 
-func insertMetadata(collection *mongo.Collection, md metadata.Metadata) bool {
-	_, e := collection.InsertOne(context.TODO(), md)
-
-	return e == nil
-}
-
-func main() {
-	mdbc, e := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
-	if e != nil {
-		panic(e)
+func hasInfoHash(InfoHash [20]byte) bool {
+	values, err := db.Query("torrents").Where(clover.Field("InfoHash").Eq(InfoHash)).FindAll()
+	if err != nil {
+		return false
 	}
-	defer func() {
-		if e = mdbc.Disconnect(context.TODO()); e != nil {
-			panic(e)
-		}
-	}()
+	return len(values) > 0
+}
 
-	tcol := mdbc.Database("torrent").Collection("metadata")
+func getInfoHashCount() int {
+	vals, _ := db.Query("torrents").FindAll()
+	return len(vals)
+}
+
+type MetaData struct {
+	Name         string
+	DiscoveredOn string
+	TotalSize    uint64
+	FileCount    uint64
+}
+
+func document2MetaData(values []*clover.Document) []MetaData {
+	rVal := make([]MetaData, len(values))
+	for i, value := range values {
+		rVal[i] = MetaData{
+			value.Get("Name").(string),
+			time.Unix(int64(value.Get("DiscoveredOn").(float64)), 0).Format(time.RFC822),
+			uint64(value.Get("TotalSize").(float64)),
+			uint64(len(value.Get("Files").([]interface{}))),
+		}
+	}
+	return rVal
+}
+
+func findByName(name string) []MetaData {
+	values, _ := db.Query("torrents").MatchPredicate(func(doc *clover.Document) bool {
+		return strings.Contains(strings.ToLower(doc.Get("Name").(string)), strings.ToLower(name))
+	}).FindAll()
+	return document2MetaData(values)
+}
+
+func insertMetadata(md metadata.Metadata) bool {
+	doc := clover.NewDocument()
+	doc.Set("Name", md.Name)
+	doc.Set("InfoHash", md.InfoHash)
+	doc.Set("Files", md.Files)
+	doc.Set("DiscoveredOn", md.DiscoveredOn)
+	doc.Set("TotalSize", md.TotalSize)
+	_, err := db.InsertOne("torrents", doc)
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func crawl() {
+	err := db.CreateCollection("torrents")
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 
 	indexerAddrs := []string{"0.0.0.0:0"}
 	interruptChan := make(chan os.Signal, 1)
@@ -45,15 +85,13 @@ func main() {
 	for stopped := false; !stopped; {
 		select {
 		case result := <-trawlingManager.Output():
-			if !hasInfoHash(tcol, result.InfoHash()) {
+			if !hasInfoHash(result.InfoHash()) {
 				metadataSink.Sink(result)
 			}
 
 		case md := <-metadataSink.Drain():
-			if insertMetadata(tcol, md) {
-				fmt.Println("Added:", md.Name)
-			} else {
-				fmt.Println("Error:", e)
+			if insertMetadata(md) {
+				fmt.Println("\t + Added:", md.Name)
 			}
 
 		case <-interruptChan:
@@ -61,4 +99,41 @@ func main() {
 			stopped = true
 		}
 	}
+}
+
+var dashboardTpl, _ = gonja.FromFile("templates/dashboard.html")
+
+func dashboard(c echo.Context) error {
+	out, _ := dashboardTpl.Execute(gonja.Context{"info_hash_count": getInfoHashCount()})
+	return c.HTML(http.StatusOK, out)
+}
+
+var searchTpl, _ = gonja.FromFile("templates/search.html")
+
+func searchGet(c echo.Context) error {
+	out, _ := searchTpl.Execute(gonja.Context{})
+	return c.HTML(http.StatusOK, out)
+}
+
+func searchPost(c echo.Context) error {
+	out, _ := searchTpl.Execute(gonja.Context{"results": findByName(c.FormValue("searchInput"))})
+	return c.HTML(http.StatusOK, out)
+}
+
+func webserver() {
+	srv := echo.New()
+	srv.GET("", dashboard)
+	srv.GET("dashboard", dashboard)
+	srv.GET("search", searchGet)
+	srv.POST("search", searchPost)
+
+	err := srv.Start(":4200")
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+	go crawl()
+	webserver()
 }
