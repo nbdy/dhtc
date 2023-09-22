ef29de854ea78df8667cc73f25960e74d8c201c8
nbdy <me@nbdy.io>
2023-09-23 00:43:33 +0200

    multiplatform compatibility (this will most likely become its own module soon)

---
+++ dhtc-client/indexingService.go
@@ -0,0 +1,271 @@
+package dhtc_client
+
+import (
+	"github.com/rs/zerolog/log"
+	"math/rand"
+	"net"
+	"sync"
+	"time"
+)
+
+type IndexingService struct {
+	// Private
+	protocol      *Protocol
+	started       bool
+	interval      time.Duration
+	eventHandlers IndexingServiceEventHandlers
+
+	nodeID []byte
+	// []byte type would be a much better fit for the keys but unfortunately (and quite
+	// understandably) slices cannot be used as keys (since they are not hashable), and using arrays
+	// (or even the conversion between each other) is a pain; hence map[string]net.UDPAddr
+	//                                                                  ^~~~~~
+	routingTable      map[string]*net.UDPAddr
+	routingTableMutex sync.RWMutex
+	maxNeighbors      uint
+
+	counter          uint16
+	getPeersRequests map[[2]byte][20]byte // GetPeersQuery.`t` -> infohash
+}
+
+type IndexingServiceEventHandlers struct {
+	OnResult func(IndexingResult)
+}
+
+type IndexingResult struct {
+	infoHash  [20]byte
+	peerAddrs []net.TCPAddr
+}
+
+func (ir IndexingResult) InfoHash() [20]byte {
+	return ir.infoHash
+}
+
+func (ir IndexingResult) PeerAddrs() []net.TCPAddr {
+	return ir.peerAddrs
+}
+
+func NewIndexingService(laddr string, interval time.Duration, maxNeighbors uint, eventHandlers IndexingServiceEventHandlers) *IndexingService {
+	service := new(IndexingService)
+	service.interval = interval
+	service.protocol = NewProtocol(
+		laddr,
+		ProtocolEventHandlers{
+			OnFindNodeResponse:         service.onFindNodeResponse,
+			OnGetPeersResponse:         service.onGetPeersResponse,
+			OnSampleInfohashesResponse: service.onSampleInfohashesResponse,
+		},
+	)
+	service.nodeID = make([]byte, 20)
+	service.routingTable = make(map[string]*net.UDPAddr)
+	service.maxNeighbors = maxNeighbors
+	service.eventHandlers = eventHandlers
+
+	service.getPeersRequests = make(map[[2]byte][20]byte)
+
+	return service
+}
+
+func (is *IndexingService) Start() {
+	if is.started {
+		log.Panic().Msg("Attempting to Start() a mainline/IndexingService that has been already started! (Programmer error.)")
+	}
+	is.started = true
+
+	is.protocol.Start()
+	go is.index()
+
+	log.Info().Msg("Indexing Service started!")
+}
+
+func (is *IndexingService) Terminate() {
+	is.protocol.Terminate()
+}
+
+func (is *IndexingService) index() {
+	for range time.Tick(is.interval) {
+		is.routingTableMutex.RLock()
+		routingTableLen := len(is.routingTable)
+		is.routingTableMutex.RUnlock()
+		if routingTableLen == 0 {
+			is.bootstrap()
+		} else {
+			log.Info().Msgf("Latest status: %i %i", routingTableLen, is.maxNeighbors)
+			//TODO
+			is.findNeighbors()
+			is.routingTableMutex.Lock()
+			is.routingTable = make(map[string]*net.UDPAddr)
+			is.routingTableMutex.Unlock()
+		}
+	}
+}
+
+func (is *IndexingService) bootstrap() {
+	bootstrappingNodes := []string{
+		"router.bittorrent.com:6881",
+		"dht.transmissionbt.com:6881",
+		"dht.libtorrent.org:25401",
+	}
+
+	log.Debug().Msg("Bootstrapping as routing table is empty...")
+	for _, node := range bootstrappingNodes {
+		target := make([]byte, 20)
+		_, err := rand.Read(target)
+		if err != nil {
+			log.Panic().Msg("Could NOT generate random bytes during bootstrapping!")
+		}
+
+		addr, err := net.ResolveUDPAddr("udp", node)
+		if err != nil {
+			log.Error().Msg("Could NOT resolve (UDP) address of the bootstrapping node!")
+			continue
+		}
+
+		is.protocol.SendMessage(NewFindNodeQuery(is.nodeID, target), addr)
+	}
+}
+
+func (is *IndexingService) findNeighbors() {
+	target := make([]byte, 20)
+
+	/*
+		We could just RLock and defer RUnlock here, but that would mean that each response that we get could not Lock
+		the table because we are sending. So we would basically make read and write NOT concurrent.
+		A better approach would be to get all addresses to send in a slice and then work on that, releasing the main map.
+	*/
+	is.routingTableMutex.RLock()
+	addressesToSend := make([]*net.UDPAddr, 0, len(is.routingTable))
+	for _, addr := range is.routingTable {
+		addressesToSend = append(addressesToSend, addr)
+	}
+	is.routingTableMutex.RUnlock()
+
+	for _, addr := range addressesToSend {
+		_, err := rand.Read(target)
+		if err != nil {
+			log.Panic().Msg("Could NOT generate random bytes during bootstrapping!")
+		}
+
+		is.protocol.SendMessage(
+			NewSampleInfohashesQuery(is.nodeID, []byte("aa"), target),
+			addr,
+		)
+	}
+}
+
+func (is *IndexingService) onFindNodeResponse(response *Message, addr *net.UDPAddr) {
+	is.routingTableMutex.Lock()
+	defer is.routingTableMutex.Unlock()
+
+	for _, node := range response.R.Nodes {
+		if uint(len(is.routingTable)) >= is.maxNeighbors {
+			break
+		}
+		if node.Addr.Port == 0 { // Ignore nodes who "use" port 0.
+			continue
+		}
+
+		is.routingTable[string(node.ID)] = &node.Addr
+
+		target := make([]byte, 20)
+		_, err := rand.Read(target)
+		if err != nil {
+			log.Panic().Msg("Could NOT generate random bytes!")
+		}
+		is.protocol.SendMessage(
+			NewSampleInfohashesQuery(is.nodeID, []byte("aa"), target),
+			&node.Addr,
+		)
+	}
+}
+
+func (is *IndexingService) onGetPeersResponse(msg *Message, addr *net.UDPAddr) {
+	var t [2]byte
+	copy(t[:], msg.T)
+
+	infoHash := is.getPeersRequests[t]
+	// We got a response, so free the key!
+	delete(is.getPeersRequests, t)
+
+	// BEP 51 specifies that
+	//     The new sample_infohashes remote procedure call requests that a remote node return a string of multiple
+	//     concatenated infohashes (20 bytes each) FOR WHICH IT HOLDS GET_PEERS VALUES.
+	//                                                                          ^^^^^^
+	// So theoretically we should never hit the case where `values` is empty, but c'est la vie.
+	if len(msg.R.Values) == 0 {
+		return
+	}
+
+	peerAddrs := make([]net.TCPAddr, 0)
+	for _, peer := range msg.R.Values {
+		if peer.Port == 0 {
+			continue
+		}
+
+		peerAddrs = append(peerAddrs, net.TCPAddr{
+			IP:   peer.IP,
+			Port: peer.Port,
+		})
+	}
+
+	is.eventHandlers.OnResult(IndexingResult{
+		infoHash:  infoHash,
+		peerAddrs: peerAddrs,
+	})
+}
+
+func (is *IndexingService) onSampleInfohashesResponse(msg *Message, addr *net.UDPAddr) {
+	// request samples
+	for i := 0; i < len(msg.R.Samples)/20; i++ {
+		var infoHash [20]byte
+		copy(infoHash[:], msg.R.Samples[i:(i+1)*20])
+
+		msg := NewGetPeersQuery(is.nodeID, infoHash[:])
+		t := uint16BE(is.counter)
+		msg.T = t[:]
+
+		is.protocol.SendMessage(msg, addr)
+
+		is.getPeersRequests[t] = infoHash
+		is.counter++
+	}
+
+	// TODO: good idea, but also need to track how long they have been here
+	//if msg.R.Num > len(msg.R.Samples) / 20 &&  time.Duration(msg.R.Interval) <= is.interval {
+	//	if addr.Port != 0 {  // ignore nodes who "use" port 0...
+	//		is.routingTable[string(msg.R.ID)] = addr
+	//	}
+	//}
+
+	// iterate
+	is.routingTableMutex.Lock()
+	defer is.routingTableMutex.Unlock()
+	for _, node := range msg.R.Nodes {
+		if uint(len(is.routingTable)) >= is.maxNeighbors {
+			break
+		}
+		if node.Addr.Port == 0 { // Ignore nodes who "use" port 0.
+			continue
+		}
+		is.routingTable[string(node.ID)] = &node.Addr
+
+		// TODO
+		/*
+			target := make([]byte, 20)
+			_, err := rand.Read(target)
+			if err != nil {
+				zap.L().Panic("Could NOT generate random bytes!")
+			}
+			is.protocol.SendMessage(
+				NewSampleInfohashesQuery(is.nodeID, []byte("aa"), target),
+				&node.Addr,
+			)
+		*/
+	}
+}
+
+func uint16BE(v uint16) (b [2]byte) {
+	b[0] = byte(v >> 8)
+	b[1] = byte(v)
+	return
+}
--- go.mod
+++ go.mod
@@ -3,20 +3,22 @@ module dhtc
 go 1.18
 
 require (
+	github.com/anacrolix/missinggo/v2 v2.7.2
 	github.com/anacrolix/torrent v1.52.5
-	github.com/boramalper/magnetico v0.12.0
 	github.com/deckarep/golang-set/v2 v2.3.1
 	github.com/gin-gonic/gin v1.9.1
+	github.com/libp2p/go-sockaddr v0.2.0
 	github.com/nikolalohinski/gonja v1.5.3
 	github.com/ostafen/clover/v2 v2.0.0-alpha.3.0.20230909104211-0726a6c7d4e5
 	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.30.0
+	github.com/willf/bloom v2.0.3+incompatible
+	golang.org/x/sys v0.12.0
 	gopkg.in/telebot.v3 v3.1.3
 )
 
 require (
 	github.com/anacrolix/missinggo v1.3.0 // indirect
-	github.com/anacrolix/missinggo/v2 v2.7.2 // indirect
 	github.com/bradfitz/iter v0.0.0-20191230175014-e8f45d346db8 // indirect
 	github.com/bytedance/sonic v1.10.1 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
@@ -46,7 +48,6 @@ require (
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
-	github.com/libp2p/go-sockaddr v0.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -59,17 +60,13 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/willf/bitset v1.1.11 // indirect
-	github.com/willf/bloom v2.0.3+incompatible // indirect
 	github.com/yargevad/filepathx v1.0.0 // indirect
 	go.etcd.io/bbolt v1.3.7 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/arch v0.5.0 // indirect
 	golang.org/x/crypto v0.13.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.15.0 // indirect
-	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
